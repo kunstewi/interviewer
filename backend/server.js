8f8abb480d2d19a958ee3f8729211abe5de2703b
kunstewi <kan.ai35423@gmail.com>
2025-11-02 19:06:56 +0530

    added authentication routes

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware to handle cors
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Connect DB
+connectDB();
+
+// Middleware to Parse JSON
+app.use(express.json());
+
+// Routes
+app.use("api/auth", authRoutes);
+
+// Server Uploads Folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
